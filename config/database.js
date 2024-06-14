76cb4d2673d861cba06e397e1206896cc1f3855a
Bruno Santos <1reddw1@gmai.com>
2024-06-14 19:01:06 -0300

    Second Commit

---
+++ config/database.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    sqlite: {
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          env('DATABASE_FILENAME', '.tmp/data.db')
+        ),
+      },
+      useNullAsDefault: true,
+    },
+    pool: {
+      min: 0,
+      max: 5
+    }
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
